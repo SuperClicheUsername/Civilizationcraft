d9cacfcd130c3ac932cadcaa6094e6cf5ff1bf73
SuperClicheUsername <makoziol0@gmail.com>
2014-07-11 17:31:18 -0400

    Initial project workspace

---
+++ src/main/java/com/superclicheusername/civilizationcraft/Civilizationcraft.java
@@ -0,0 +1,8 @@
+package com.superclicheusername.civilizationcraft;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = "civilizationcraft", name = "Civilizationcraft", version = "1.0")
+public class Civilizationcraft
+{
+}
