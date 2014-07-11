1bc0925c40776e88d4c1cede6073a21b9d858786
SuperClicheUsername <makoziol0@gmail.com>
2014-07-11 18:35:15 -0400

    Added a reference class

--- src/main/java/com/superclicheusername/civilizationcraft/Civilizationcraft.java
+++ src/main/java/com/superclicheusername/civilizationcraft/Civilizationcraft.java
@@ -1,31 +1,33 @@
 package com.superclicheusername.civilizationcraft;
 
+import com.superclicheusername.civilizationcraft.proxy.IProxy;
+import com.superclicheusername.civilizationcraft.reference.Reference;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "civilizationcraft", name = "Civilizationcraft", version = "1.7.10-1.0")
-public class Civilizationcraft
-{
-    @Mod.Instance("civilizationcraft")
-    public static Civilizationcraft instance;
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
+public class Civilizationcraft {
+	@Mod.Instance(Reference.MOD_ID)
+	public static Civilizationcraft instance;
 
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS, modId = Reference.MOD_ID)
+	public static IProxy proxy;
 
-    }
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
 
-    @Mod.EventHandler
-    public void Init(FMLInitializationEvent event)
-    {
+	}
 
-    }
+	@Mod.EventHandler
+	public void Init(FMLInitializationEvent event) {
 
-    @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent event)
-    {
+	}
 
-    }
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+
+	}
 }
---
+++ src/main/java/com/superclicheusername/civilizationcraft/reference/Reference.java
@@ -0,0 +1,10 @@
+package com.superclicheusername.civilizationcraft.reference;
+
+public class Reference
+{
+	public static final String MOD_ID = "civilizationcraft";
+	public static final String MOD_NAME = "Civilizationcraft";
+	public static final String MOD_VERSION = "1.7.10-1.0";
+	public static final String SERVER_PROXY_CLASS = "com.superclicheusername.civilizationcraft.proxy.ServerProxy";
+	public static final String CLIENT_PROXY_CLASS = "com.superclicheusername.civilizationcraft.proxy.ClientProxy";
+}
