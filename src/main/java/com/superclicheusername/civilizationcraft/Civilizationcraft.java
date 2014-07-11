ce11b34583a881c1899d7da06d62a5db841d4de9
SuperClicheUsername <makoziol0@gmail.com>
2014-07-11 17:43:16 -0400

    Mod class!

--- src/main/java/com/superclicheusername/civilizationcraft/Civilizationcraft.java
+++ src/main/java/com/superclicheusername/civilizationcraft/Civilizationcraft.java
@@ -1,8 +1,31 @@
 package com.superclicheusername.civilizationcraft;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "civilizationcraft", name = "Civilizationcraft", version = "1.0")
+@Mod(modid = "civilizationcraft", name = "Civilizationcraft", version = "1.7.10-1.0")
 public class Civilizationcraft
 {
+    @Mod.Instance("civilizationcraft")
+    public static Civilizationcraft instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
 }
